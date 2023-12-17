0ae8855b2a33292af12f190a4071ace88a558732
nikit <nglaz12@yandex.ru>
2023-12-17 17:20:45 +0700

    Add 30pr

--- app/src/main/java/com/example/pr30_glazirinng/MainActivity.java
+++ app/src/main/java/com/example/pr30_glazirinng/MainActivity.java
@@ -1,26 +1,125 @@
 package com.example.pr30_glazirinng;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-    final String LOG_TAG = "myLogs1234";
+    final String LOG_TAG = "myLogs";
+    final int TASK1_CODE = 1;
+    final int TASK2_CODE = 2;
+    final int TASK3_CODE = 3;
+    final Date startTime = new Date(); // Время старта задачи
+    TextView tvTask1;
+    TextView tvTask2;
+    TextView tvTask3;
+    ExecutorService executorService;
+    DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTask1 = findViewById(R.id.tvTask1);
+        tvTask2 = findViewById(R.id.tvTask2);
+        tvTask3 = findViewById(R.id.tvTask3);
+
+        executorService = Executors.newFixedThreadPool(2);
     }
-    public void onClickStart(View v) {
-        startService(new Intent(this, MyService.class));
-        Log.d(LOG_TAG, "Service started");
+    public void onClickButton1(View view) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(LOG_TAG, "Task1: Started");
+                try {
+                    TimeUnit.SECONDS.sleep(8);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Date endTime = new Date(); // Время завершения задачи
+                        long executionTimeMs = endTime.getTime() - startTime.getTime(); // Разница во времени в миллисекундах
+                        long executionTimeSec = executionTimeMs / 1000; // время выполнения в секундах
+                        String formattedExecutionTime = decimalFormat.format(executionTimeSec);
+                        tvTask1.setText("Task1: Completed " + formattedExecutionTime + " sec");
+                        Log.d(LOG_TAG, "Task1: Completed in " + formattedExecutionTime + " sec");
+                    }
+                });
+            }
+        });
     }
 
-    public void onClickStop(View v) {
-        stopService(new Intent(this, MyService.class));
-        Log.d(LOG_TAG, "Service stopped");
+    public void onClickButton2(View view) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(LOG_TAG, "Task2: Started");
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Date endTime = new Date(); // Время завершения задачи
+                        long executionTimeMs = endTime.getTime() - startTime.getTime(); // Разница во времени в миллисекундах
+                        long executionTimeSec = executionTimeMs / 1000; // время выполнения в секундах
+                        String formattedExecutionTime = decimalFormat.format(executionTimeSec);
+                        tvTask2.setText("Task2: Completed "+ formattedExecutionTime + " sec");
+                        Log.d(LOG_TAG, "Task2: Completed in " + formattedExecutionTime + " sec");
+                    }
+                });
+            }
+        });
+    }
+
+    public void onClickButton3(View view) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(LOG_TAG, "Task3: Started");
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Date endTime = new Date(); // Время завершения задачи
+                        long executionTimeMs = endTime.getTime() - startTime.getTime(); // Разница во времени в миллисекундах
+                        long executionTimeSec = executionTimeMs / 1000; // время выполнения в секундах
+                        String formattedExecutionTime = decimalFormat.format(executionTimeSec);
+                        tvTask3.setText("Task3: Completed "+ formattedExecutionTime + " sec");
+                        Log.d(LOG_TAG, "Task3: Completed in " + formattedExecutionTime + " sec");
+                    }
+                });
+            }
+        });
+    }
+
+    public void onClickButtonClear(View view) {
+        tvTask1.setText("Task1: "); // Очистка текста
+        tvTask2.setText("Task2: "); // Очистка текста
+        tvTask3.setText("Task3: "); // Очистка текста
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        executorService.shutdown();
     }
 }
--- app/src/main/java/com/example/pr30_glazirinng/MyService.java
+++ app/src/main/java/com/example/pr30_glazirinng/MyService.java
@@ -5,46 +5,47 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class MyService extends Service {
-    final String LOG_TAG = "myLogs";
-
+    private ThreadPoolExecutor executor;
+    private static final String LOG_TAG = "MyService";
+    @Override
     public void onCreate() {
-        Log.d(LOG_TAG, "onCreate");
         super.onCreate();
-    }
-
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+    }  @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(LOG_TAG, "onStartCommand");
-        someTask();
-        return super.onStartCommand(intent, flags, startId);
+        // Выполнение задачи
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(LOG_TAG, "ServiceTask: Started");
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d(LOG_TAG, "ServiceTask: Completed");
+            }
+        });
+
+        return START_NOT_STICKY;
     }
 
-    public void onDestroy() {
-        Log.d(LOG_TAG, "onDestroy");
-        super.onDestroy();
-
-    }
-
+    @Nullable
+    @Override
     public IBinder onBind(Intent intent) {
-        Log.d(LOG_TAG, "onBind");
         return null;
     }
 
-    void someTask() {
-        new Thread(new Runnable() {
-            public void run() {
-                for (int i = 1; i<=5; i++) {
-                    Log.d(LOG_TAG, "i = " + i);
-                    try {
-                        TimeUnit.SECONDS.sleep(1);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                stopSelf();
-            }
-        }).start();
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        executor.shutdown();
     }
 }
